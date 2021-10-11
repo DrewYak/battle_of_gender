9b94e070b4b0ed5fded28b944d16175f0f623c24
DrewYak7 <DrewYak7@gmail.com>
2021-10-11 12:52:16 +0300

    Добавлено поле scene в классе GameView

--- battle_of_ten.py
+++ battle_of_ten.py
@@ -106,8 +106,8 @@ class GameView(arcade.View):
     def __init__(self):
         # super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         super().__init__()
-        self.cell_list = None
-        self.cell_sprite = None
+
+        self.scene = None
 
         self.warrior_list = None
         self.warrior_sprite = None
@@ -115,68 +115,78 @@ class GameView(arcade.View):
         self.window.background_color = (217, 205, 175)
 
     def setup(self):
-        self.cell_list = arcade.SpriteList()
+        self.scene = arcade.Scene()
+
+        self.scene.add_sprite_list("Cells")
+        self.scene.add_sprite_list("Warriors")
+
         img = "images/cell.png"
         for i in range(SIZE):
             for j in range(SIZE):
-                self.cell_sprite = arcade.Sprite(img, SPRITE_SCALING_CELL)
-                self.cell_sprite.center_x = SHIFT + 50 + 100 * i
-                self.cell_sprite.center_y = 50 + 100 * j
-                self.cell_list.append(self.cell_sprite)
+                cell_sprite = arcade.Sprite(img, SPRITE_SCALING_CELL)
+                cell_sprite.center_x = SHIFT + 50 + 100 * i
+                cell_sprite.center_y = 50 + 100 * j
+                self.scene.add_sprite("Cells", cell_sprite)
 
         self.warrior_list = arcade.SpriteList()
 
         img = "images/T1_P_128.png"
-        self.warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
-        self.warrior_sprite.center_x = SHIFT + 50
-        self.warrior_sprite.center_y = 50
-        self.warrior_sprite.new_center_x = SHIFT + 50
-        self.warrior_sprite.new_center_y = 50
-        self.warrior_list.append(self.warrior_sprite)
+        warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
+        warrior_sprite.center_x = SHIFT + 50
+        warrior_sprite.center_y = 50
+        warrior_sprite.new_center_x = SHIFT + 50
+        warrior_sprite.new_center_y = 50
+        self.warrior_list.append(warrior_sprite)
+        self.scene.add_sprite("Warriors", warrior_sprite)
 
         img = "images/T1_A_128.png"
-        self.warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
-        self.warrior_sprite.center_x = SHIFT + 50
-        self.warrior_sprite.center_y = 150
-        self.warrior_sprite.new_center_x = SHIFT + 150
-        self.warrior_sprite.new_center_y = 150
-        self.warrior_list.append(self.warrior_sprite)
+        warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
+        warrior_sprite.center_x = SHIFT + 50
+        warrior_sprite.center_y = 150
+        warrior_sprite.new_center_x = SHIFT + 150
+        warrior_sprite.new_center_y = 150
+        self.warrior_list.append(warrior_sprite)
+        self.scene.add_sprite("Warriors", warrior_sprite)
 
         img = "images/T1_W_128.png"
-        self.warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
-        self.warrior_sprite.center_x = SHIFT + 50
-        self.warrior_sprite.center_y = 250
-        self.warrior_sprite.new_center_x = SHIFT + 50
-        self.warrior_sprite.new_center_y = 250
-        self.warrior_list.append(self.warrior_sprite)
+        warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
+        warrior_sprite.center_x = SHIFT + 50
+        warrior_sprite.center_y = 250
+        warrior_sprite.new_center_x = SHIFT + 50
+        warrior_sprite.new_center_y = 250
+        self.warrior_list.append(warrior_sprite)
+        self.scene.add_sprite("Warriors", warrior_sprite)
 
         img = "images/T2_P_128.png"
-        self.warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
-        self.warrior_sprite.center_x = SHIFT + 750
-        self.warrior_sprite.center_y = 50
-        self.warrior_sprite.new_center_x = SHIFT + 750
-        self.warrior_sprite.new_center_y = 50
-        self.warrior_list.append(self.warrior_sprite)
+        warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
+        warrior_sprite.center_x = SHIFT + 750
+        warrior_sprite.center_y = 50
+        warrior_sprite.new_center_x = SHIFT + 750
+        warrior_sprite.new_center_y = 50
+        self.warrior_list.append(warrior_sprite)
+        self.scene.add_sprite("Warriors", warrior_sprite)
 
         img = "images/T2_A_128.png"
-        self.warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
-        self.warrior_sprite.center_x = SHIFT + 750
-        self.warrior_sprite.center_y = 150
-        self.warrior_sprite.new_center_x = SHIFT + 750
-        self.warrior_sprite.new_center_y = 150
-        self.warrior_list.append(self.warrior_sprite)
+        warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
+        warrior_sprite.center_x = SHIFT + 750
+        warrior_sprite.center_y = 150
+        warrior_sprite.new_center_x = SHIFT + 750
+        warrior_sprite.new_center_y = 150
+        self.warrior_list.append(warrior_sprite)
+        self.scene.add_sprite("Warriors", warrior_sprite)
 
         img = "images/T2_W_128.png"
-        self.warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
-        self.warrior_sprite.center_x = SHIFT + 750
-        self.warrior_sprite.center_y = 250
-        self.warrior_sprite.new_center_x = SHIFT + 750
-        self.warrior_sprite.new_center_y = 50
-        self.warrior_list.append(self.warrior_sprite)
+        warrior_sprite = WarriorWithHealth(img, SPRITE_SCALING_WARRIOR, MAX_HEALTH)
+        warrior_sprite.center_x = SHIFT + 750
+        warrior_sprite.center_y = 250
+        warrior_sprite.new_center_x = SHIFT + 750
+        warrior_sprite.new_center_y = 50
+        self.warrior_list.append(warrior_sprite)
+        self.scene.add_sprite("Warriors", warrior_sprite)
 
     def on_draw(self):
         arcade.start_render()
-        self.cell_list.draw()
+        self.scene.draw()
         self.warrior_list.draw()
 
         for w in self.warrior_list:
@@ -188,7 +198,6 @@ class GameView(arcade.View):
             self.warrior_list[0].go_to(x // 100 * 100 + 50, y // 100 * 100 + 50)
 
     def on_update(self, delta_time):
-        self.cell_list.update()
         self.warrior_list.update()
 
         if self.warrior_list[0].center_x == 750 + SHIFT:
